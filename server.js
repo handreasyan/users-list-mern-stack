59416e1be5edf91db3e156a85676da0d71098e88
Hakob <andreasyanh98@gmail.com>
2026-02-15 12:18:38 +0400

    added favicon

--- server.js
+++ server.js
@@ -1,1 +1,48 @@
-// test
+const http = require(`http`)
+const path = require('path');
+const fs = require('fs');
+
+let reqCount = 0
+
+const server = http.createServer((req, res) => {
+   if(routes.includes(req.url)) {
+       reqCount++
+   }
+
+
+   switch (req.url){
+        case '/':
+            res.write(`home`)
+            break
+        case '/students':
+            res.write(`Students`)
+            break
+        case '/favicon.ico':
+            const filePath = path.join(__dirname, '','favicon.ico')
+            fs.readFile(filePath, (err, data) => {
+                if(err){
+                    res.writeHead(404)
+                    res.end()
+                    return
+                }
+
+                res.writeHead(200, {'Content-Type': 'image/x-icon'})
+                res.end(data)
+
+            })
+            return
+        case '/courses':
+            res.write(`Frontend & Backend`)
+            break
+       default:
+           res.write(`Not Found`)
+   }
+
+    res.write(` \n Requests Count: ${reqCount}`)
+
+    res.end()
+})
+
+const routes =['/','/students','/courses']
+
+server.listen(3003)
